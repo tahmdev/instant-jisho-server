39d071488068008117316070e6d323552f1efcec
tahmdev <qwertiimusic@gmail.com>
2022-05-11 23:42:50 +0200

    Added radical lookup logic

--- routes/jisho.js
+++ routes/jisho.js
@@ -6,15 +6,12 @@ router.use(cors("http://localhost:3000/", "http://192.168.178.22:3000/"));
 
 let jisho = require("../public/JMDict_e.json")
 let examples = require("../public/tatoeba-jp.json")
+let strokeCounts = require("../public/kTotalStrokes.json")
+let radk = require("../public/radk.json")
 
 router.get('/word/:query', function(req, res, next) {
   let query = req.params.query
-  let results = queryJE(query)
-  res.send(results) 
-});
-router.get('/test/:query', function(req, res, next) {
-  let query = req.params.query
-  let results = queryJE(query)
+  let results = queryJisho(query)
   res.send(results) 
 });
 
@@ -25,9 +22,67 @@ router.get("/example/:type/:query", function(req, res, next) {
   if (type === "tatoeba") {
     res.send(examples.filter(entry=> entry.text.includes(query)))
   }
+}) 
+
+router.post("/radical", function(req, res, next) {
+  
+  //takes an array of arrays and returns one array containing all mutual children
+  //by checking whether an item appears as many times as there are arrays
+  const getMutualChildren = (initArray) => {
+    if (initArray.length > 1){
+      const getAmount = (array, entry) => {
+        return array.filter(item => item === entry).length
+      }
+      
+      const removeDuplicates = (array) => {
+        return [...new Set(array)]
+      }
+  
+      return (
+        [...removeDuplicates(initArray
+          .map(item => removeDuplicates(item))
+          .flat()
+          .filter((item, idx, arr) => getAmount(arr, item) === initArray.length
+          ))
+        ]
+      )
+    }
+    else return initArray.flat()
+  }
+
+  const sortByStrokeCount = (array) => {
+    array.sort((a, b) => {
+      return strokeCounts[a][0] === strokeCounts[b][0]
+      ? 0
+      : strokeCounts[a][0] > strokeCounts[b][0]
+      ? 1
+      : -1
+    })
+    array = array.map(i => {
+      return {
+        kanji: i,
+        strokes: strokeCounts[i][0]
+      }
+    })
+    return array
+  }
+  
+  const findEnabled = (arrayOfMutualChildren) => {
+    return Object.keys(radk).filter(key => radk[key].kanji.some(kanji => arrayOfMutualChildren.includes(kanji))
+    )
+  }
+  
+  
+  let mutualChildren = getMutualChildren(req.body.map(key => radk[key].kanji))
+  res.send(
+    {
+      mutualChildren: sortByStrokeCount(mutualChildren),
+      enabled: req.body.length > 0 ? findEnabled(mutualChildren) : Object.keys(radk)
+    }
+  )
 })
 
-const queryJE = (query) => {
+const queryJisho = (query) => {
   let exact = []
   let includes = []
   let prefix = []
